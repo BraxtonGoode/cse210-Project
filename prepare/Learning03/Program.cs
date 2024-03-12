6ddee73612719284a081ca64b8bb3ea453a9c31f
BraxtonGoode <tjbgoode@gmail.com>
2024-03-12 13:58:05 -0700

    learning activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        //Fraction fraction = new Fraction();        
+        //Fraction whole = new Fraction(1);        
+        Fraction topandBottom = new Fraction(1,3);
+
+        Console.WriteLine(topandBottom.GetFractionString());
+        Console.WriteLine(topandBottom.GetDeciamlValue());
     }
 }
