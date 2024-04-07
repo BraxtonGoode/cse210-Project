a79e3a19242f743e0a4b09f9af25db91d0855a40
BraxtonGoode <tjbgoode@gmail.com>
2024-04-06 17:00:57 -0700

    changes

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -10,3 +10,11 @@ class Program
         eManager.Start();
     }
 }
+
+
+
+/*Progress so far:
+For my Final Project so far i have been able to create all 8 classes, 4 of them are are in a parent child rlationship so this features Inheritance and polymorphism because the child 
+methods can overide the parents. All of my classes have a section for variables, Constructors and methods that allow the class to have specific responsibilities
+so this goes along with the idea of abstraction. My assignment also features encapsulation with in the classes by using Private and protected variables and private 
+and public methods when they are necessary. The Project is now able to function in the way that i wish but the interface and error handling still needs to get improved.   */ 
